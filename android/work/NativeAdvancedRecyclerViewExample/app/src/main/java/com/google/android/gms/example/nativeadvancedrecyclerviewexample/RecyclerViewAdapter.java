ddd155e0c8f87aae7daa8a5091334faedd2de8b7
Valery Petrov <valeryvpetrov.dev@gmail.com>
2020-02-19 16:14:03 +0300

    Add native add view holder

--- android/work/NativeAdvancedRecyclerViewExample/app/src/main/java/com/google/android/gms/example/nativeadvancedrecyclerviewexample/RecyclerViewAdapter.java
+++ android/work/NativeAdvancedRecyclerViewExample/app/src/main/java/com/google/android/gms/example/nativeadvancedrecyclerviewexample/RecyclerViewAdapter.java
@@ -15,12 +15,18 @@
  */
 package com.google.android.gms.example.nativeadvancedrecyclerviewexample;
 
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
+
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.List;
@@ -32,6 +38,9 @@ import java.util.List;
 class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     // A menu item view type.
     private static final int MENU_ITEM_VIEW_TYPE = 0;
+
+    // A native add view type.
+    private static final int UNIFIED_NATIVE_AD_VIEW_TYPE = 1;
 
     // An Activity's Context.
     private final Context mContext;
@@ -79,6 +88,10 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
      */
     @Override
     public int getItemViewType(int position) {
+        Object recyclerViewItem = mRecyclerViewItems.get(position);
+        if (recyclerViewItem instanceof UnifiedNativeAd) {
+            return UNIFIED_NATIVE_AD_VIEW_TYPE;
+        }
         return MENU_ITEM_VIEW_TYPE;
     }
 
@@ -87,9 +100,19 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
      */
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        View menuItemLayoutView = LayoutInflater.from(viewGroup.getContext()).inflate(
-                R.layout.menu_item_container, viewGroup, false);
-        return new MenuItemViewHolder(menuItemLayoutView);
+        switch (viewType) {
+            case UNIFIED_NATIVE_AD_VIEW_TYPE:
+                View unifiedNativeLayoutView = LayoutInflater.from(
+                    viewGroup.getContext()).inflate(R.layout.ad_unified,
+                    viewGroup, false);
+                return new UnifiedNativeAdViewHolder(unifiedNativeLayoutView);
+            case MENU_ITEM_VIEW_TYPE:
+                // Fall through.
+            default:
+                View menuItemLayoutView = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.menu_item_container, viewGroup, false);
+                return new MenuItemViewHolder(menuItemLayoutView);
+        }
     }
 
     /**
@@ -98,19 +121,80 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
      */
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        MenuItemViewHolder menuItemHolder = (MenuItemViewHolder) holder;
-        MenuItem menuItem = (MenuItem) mRecyclerViewItems.get(position);
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case UNIFIED_NATIVE_AD_VIEW_TYPE:
+                UnifiedNativeAd nativeAd = (UnifiedNativeAd) mRecyclerViewItems.get(position);
+                populateNativeAdView(nativeAd, ((UnifiedNativeAdViewHolder) holder).getAdView());
+                break;
+            case MENU_ITEM_VIEW_TYPE:
+                // fall through
+            default:
+                MenuItemViewHolder menuItemHolder = (MenuItemViewHolder) holder;
+                MenuItem menuItem = (MenuItem) mRecyclerViewItems.get(position);
 
-        // Get the menu item image resource ID.
-        String imageName = menuItem.getImageName();
-        int imageResID = mContext.getResources().getIdentifier(imageName, "drawable",
-                mContext.getPackageName());
+                // Get the menu item image resource ID.
+                String imageName = menuItem.getImageName();
+                int imageResID = mContext.getResources().getIdentifier(imageName, "drawable",
+                    mContext.getPackageName());
 
-        // Add the menu item details to the menu item view.
-        menuItemHolder.menuItemImage.setImageResource(imageResID);
-        menuItemHolder.menuItemName.setText(menuItem.getName());
-        menuItemHolder.menuItemPrice.setText(menuItem.getPrice());
-        menuItemHolder.menuItemCategory.setText(menuItem.getCategory());
-        menuItemHolder.menuItemDescription.setText(menuItem.getDescription());
+                // Add the menu item details to the menu item view.
+                menuItemHolder.menuItemImage.setImageResource(imageResID);
+                menuItemHolder.menuItemName.setText(menuItem.getName());
+                menuItemHolder.menuItemPrice.setText(menuItem.getPrice());
+                menuItemHolder.menuItemCategory.setText(menuItem.getCategory());
+                menuItemHolder.menuItemDescription.setText(menuItem.getDescription());
+        }
+    }
+
+    private void populateNativeAdView(UnifiedNativeAd nativeAd,
+                                      UnifiedNativeAdView adView) {
+        // Some assets are guaranteed to be in every UnifiedNativeAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeAd.getBody());
+        ((Button) adView.getCallToActionView()).setText(nativeAd.getCallToAction());
+
+        // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
+        // check before trying to display them.
+        NativeAd.Image icon = nativeAd.getIcon();
+
+        if (icon == null) {
+            adView.getIconView().setVisibility(View.INVISIBLE);
+        } else {
+            ((ImageView) adView.getIconView()).setImageDrawable(icon.getDrawable());
+            adView.getIconView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getPrice() == null) {
+            adView.getPriceView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getPriceView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getPriceView()).setText(nativeAd.getPrice());
+        }
+
+        if (nativeAd.getStore() == null) {
+            adView.getStoreView().setVisibility(View.INVISIBLE);
+        } else {
+            adView.getStoreView().setVisibility(View.VISIBLE);
+            ((TextView) adView.getStoreView()).setText(nativeAd.getStore());
+        }
+
+        if (nativeAd.getStarRating() == null) {
+            adView.getStarRatingView().setVisibility(View.INVISIBLE);
+        } else {
+            ((RatingBar) adView.getStarRatingView())
+                .setRating(nativeAd.getStarRating().floatValue());
+            adView.getStarRatingView().setVisibility(View.VISIBLE);
+        }
+
+        if (nativeAd.getAdvertiser() == null) {
+            adView.getAdvertiserView().setVisibility(View.INVISIBLE);
+        } else {
+            ((TextView) adView.getAdvertiserView()).setText(nativeAd.getAdvertiser());
+            adView.getAdvertiserView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeAd);
     }
 }
